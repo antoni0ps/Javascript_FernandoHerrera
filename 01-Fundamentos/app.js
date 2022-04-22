c1c5bb05cc417475ddc8bb8c7ce635b4c3838463
antoni0ps <anpisan@gmail.com>
2022-04-22 09:43:04 +0200

    first commit

---
+++ 01-Fundamentos/app.js
@@ -0,0 +1,29 @@
+// console.log('Hola mundo');
+
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman'
+    x = a + b;
+
+const saludo = c + d;
+console.log(saludo)
+
+
+console.log('%c Mis Variables', 'color:green; font-weight:bold')
+
+console.log({a});
+console.log({b});
+console.log({c});
+
+
+//Podemos imprimir normal, con warning o error
+// console.log(c)
+// console.warn({c})
+// console.error(c)
+
+
+//console.table({a,b,c,d,x})
+
+
