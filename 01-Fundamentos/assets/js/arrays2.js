e2277c4c6f6fac33b5de08eea0c9d73482172096
antoni0ps <anpisan@gmail.com>
2022-04-25 09:37:26 +0200

    objetos literales

--- 01-Fundamentos/assets/js/arrays2.js
+++ 01-Fundamentos/assets/js/arrays2.js
@@ -1,7 +1,7 @@
 // PROPIEDADES Y MÉTODOS BÁSICOS DE LOS ARRAYS
 
-let juegos = ['Zelda', 'Mario', 'Metroid', 'Kirby'];
-console.log('Largo: ', juegos.length);
+let juegos = ["Zelda", "Mario", "Metroid", "Kirby"];
+console.log("Largo: ", juegos.length);
 
 let primero = juegos[0];
 let ultimo = juegos[juegos.length - 1];
@@ -12,23 +12,23 @@ juegos.forEach((elemento, indice, arr) => {
   console.log({ elemento, indice, arr });
 });
 
+let nuevaLongitud = juegos.push("F-Zero"); //push para añadir elemento al final del array
+console.log({ nuevaLongitud }, { juegos });
 
-let nuevaLongitud = juegos.push('F-Zero');  //push para añadir elemento al final del array
-console.log({nuevaLongitud}, {juegos});
+nuevaLongitud = juegos.unshift("Star Fox"); //unshift para añadir elemento al principio del array (star-fox)
+console.log({ nuevaLongitud }, { juegos });
 
-nuevaLongitud = juegos.unshift('Star Fox');  //unshift para añadir elemento al principio del array
-console.log({nuevaLongitud}, {juegos});
+let juegoBorrado = juegos.pop(); // pop para borrar el ultimo elemento del array (F-Zero)
+console.log({ juegoBorrado, juegos });
 
-
-let juegoBorrado = juegos.pop()   // pop para borrar el ultimo elemento del array
-console.log({juegoBorrado, juegos});
-
-
-let pos = 3;  //posicion 3, Metroid
-juegos.splice(pos, 1)   //splice para borrar elementos a partir de una posicion inicial, en este caso se borra un elemento a partir de la posicion 3
+let pos = 3; //posicion 3, Metroid
+juegos.splice(pos, 1); //splice para borrar elementos a partir de una posicion inicial, en este caso se borra un elemento a partir de la posicion 3
 
 console.log(juegos);
 
-let metroidIndex = juegos.indexOf('Zelda')
-console.log({metroidIndex});
+juegos.forEach((juego) => {
+  console.log(juego);
+});
 
+let zeldaIndex = juegos.indexOf("Zelda");
+console.log({ ZeldaIndex: zeldaIndex });
---
+++ 01-Fundamentos/assets/js/objeto-literal.js
@@ -0,0 +1,71 @@
+
+// OBJETOS LITERALES
+
+let personaje = { //Cuando se vean llaves de apertura y cierre asociamos a un objeto, los literales tienen pares de valores (clave, valor)
+    nombre:'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    coords:{
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10880, 90265',
+        ubicacion: 'Malibu, California'
+    },
+    'ultima pelicula': 'Infinity War'    //Para declarar propiedades con espacio o guión se ponen entre comilla simples
+};    
+
+//diferentes formas de acceder a los atributos del objeto
+console.log('Nombre:', personaje.nombre);
+console.log('Nombre:', personaje['nombre']);
+console.log('Edad', personaje.edad);
+
+console.log('Coordenadas:', personaje.coords);
+console.log('Latitud:', personaje.coords.lat);
+console.log('Longitud:', personaje.coords.lng);
+
+console.log('Numero de trajes:', personaje.trajes.length);
+console.log('Ultimo traje:', personaje.trajes[personaje.trajes.length-1]);
+
+const x = 'vivo';           //Creamos variable o constante cuyo nombre coincide con una propiedad
+console.log(personaje[x]);  //Llamamos a esa propiedad mediante la notación con corchete
+
+console.log('Ultima pelicula:', personaje['ultima pelicula']); //Para llamar a propiedades con espacio o guion se llaman com comilla simple
+
+
+// MÁS DETALLES
+
+delete personaje.edad;          //delete para borrar propiedades de un objeto
+console.log(personaje);
+
+
+personaje.casado = true;       //para agregar una nueva propiedad
+personaje.gafas = 'Azules';
+
+
+const entriesPares = Object.entries(personaje);   //Object.entries para convertir el objeto a array de pares de valores (nombre:, Tony Stark)
+console.log(entriesPares);
+
+
+Object.freeze(personaje);    //Object.freeze para bloquear cambios en el objeto y hacerlo inmutable
+
+personaje.dinero = 10000000000;  //no funciona
+
+
+const propiedades = Object.getOwnPropertyNames(personaje);   //Para obtener las propiedades del objeto en forma de array
+console.log(propiedades);
+
+const valores = Object.values(personaje);    //Para obtener los valores de las propiedades del objeto
+console.log(valores);
+
+
+
+
+
+
+
+
+
