bc3072204ea66ababa303608ee60b0e280b81dc2
Antonio <anpisan@gmail.com>
2022-05-03 17:35:52 +0200

    finished todo

--- 04-TODO/src/index.js
+++ 04-TODO/src/index.js
@@ -3,15 +3,15 @@ import { Todo, TodoList } from './classes'; //Importaciones de clases
 import { crearTodoHtml } from './js/componentes';
 
 
-
 export const todoList = new TodoList();
 
-const todo1 = new Todo('Comprar un dinosaurio');
 
-todoList.nuevoTodo(todo1);
+todoList.todoList.forEach(todo => crearTodoHtml(todo));
+
+// const todo1 = new Todo('Comprar un dinosaurio');
+// todoList.nuevoTodo(todo1);
+// console.log(todoList);
+// crearTodoHtml(todo1);
 
 
-
-console.log(todoList);
-
-crearTodoHtml(todo1);
+// localStorage.setItem('mi-key', 'ABC123');
--- 04-TODO/src/js/componentes.js
+++ 04-TODO/src/js/componentes.js
@@ -5,6 +5,8 @@ import { todoList } from "../index"
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
 const btnBorrarCompletados = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
 
 
@@ -71,6 +73,36 @@ btnBorrarCompletados.addEventListener('click', (event) => {
         if (elemento.classList.contains('completed')) {
             divTodoList.removeChild(elemento);
 
+        }
+    }
+})
+
+ulFiltros.addEventListener('click', (event) => {
+
+    const filtro = event.target.text;
+    if (!filtro) return;
+
+    anchorFiltros.array.forEach(element => element.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
         }
     }
 })
